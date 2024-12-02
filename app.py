b7babcdad6e73d0f7c0eeb4e3c5655c3f86149aa
rkdbq <rkdbg11752@jbnu.ac.kr>
2024-12-02 12:31:46 +0900

    open mysql server to public IP

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask, request, jsonify, abort
 from models import db, User
 
+# * 포트 포워딩 정보
+#   - 113.198.66.67:10xxx -> 10.0.0.xxx:8080
+#   - 113.198.66.67:13xxx -> 10.0.0.xxx:3000
+#   - 113.198.66.67:17xxx -> 10.0.0.xxx:443
+#   - 113.198.66.67:18xxx -> 10.0.0.xxx:80
+#   - 113.198.66.67:19xxx -> 10.0.0.xxx:7777 (ssh)
+#   - 위 포트 외는 방화벽으로 차단되어 있습니다.
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://rkdbq_local:Kang1293!!@localhost/wsd3_db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://rkdbq:Kang1293!!@localhost:3000/wsd3_db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
@@ -48,5 +56,5 @@ if __name__ == '__main__':
     app.run(
         debug=True,
         host='0.0.0.0',
-        port=3000
+        port=8080
     )
