e328c05b433de5959ef86cd8729986e7bda26a1b
rkdbq <rkdbg11752@jbnu.ac.kr>
2024-12-12 20:27:52 +0900

    배포 작업

---
+++ README.md
@@ -0,0 +1,18 @@
+## 실행 방법
+
+### 패키지 설치
+```
+pip install requirements.txt
+```
+
+### 환경변수 설정
+```
+export SECRET_KEY=<YOUR SECRET KEY TO JWT ENCODE>
+export DB_NAME=<DB USER NAME>
+export DB_PASSWORD=<DB USER PASSWORD>
+```
+
+### 실행
+```
+python app.py
+```
--- app.py
+++ app.py
@@ -1,15 +1,8 @@
+from waitress import serve
 from flask import Flask
 from flasgger import Swagger
 from models import db
 from controllers import register_blueprints
-
-# * 포트 포워딩 정보
-#   - 113.198.66.67:10xxx -> 10.0.0.xxx:8080
-#   - 113.198.66.67:13xxx -> 10.0.0.xxx:3000
-#   - 113.198.66.67:17xxx -> 10.0.0.xxx:443
-#   - 113.198.66.67:18xxx -> 10.0.0.xxx:80
-#   - 113.198.66.67:19xxx -> 10.0.0.xxx:7777 (ssh)
-#   - 위 포트 외는 방화벽으로 차단되어 있습니다.
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
@@ -29,8 +22,8 @@ def home():
     return "Server is online"
 
 if __name__ == '__main__':
-    app.run(
-        debug=True,
-        host='0.0.0.0',
-        port=8080
+    serve(
+        app=app,
+        host="0.0.0.0",
+        port=8080,
     )
